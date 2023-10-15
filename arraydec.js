19fed35e7d88af945febcad0de9d08aa3a239f0c
Lakshmi-Tamatam <147979041+Lakshmi-Tamatam@users.noreply.github.com>
2023-10-15 20:06:42 +0530

    Add files via upload

---
+++ arraydec.js
@@ -0,0 +1,5 @@
+//let input = prompt("enter a list of values"); this is fro user input but prompt is not working in vscode 
+//let array = input.split(",");
+const array = ['7','4','2','5','1','6'];
+const descArr = array.sort().reverse();
+console.log(descArr);
